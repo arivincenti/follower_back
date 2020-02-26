b984f9dd3e1b9600f809f2fda70c30e7adb025a8
Ari Vincenti <arivincenti@gmail.com>
2020-02-26 12:17:04 -0300

    cambio en configuracion de bbdd

--- server/database/database.ts
+++ server/database/database.ts
@@ -1,26 +1,32 @@
 import Mongoose from "mongoose";
 
-export default class Database {
-    private URI: string = "mongodb://localhost:27017/follower";
-    // private URI = 'mongodb+srv://follower:amFjt2afBZ4wj5ru@cluster0-jfhtw.mongodb.net/follower?retryWrites=true&w=majority';
-    constructor() {
+export default class Database
+{
+    // private URI: string = "mongodb://localhost:27017/follower";
+    private URI = 'mongodb+srv://follower:amFjt2afBZ4wj5ru@cluster0-jfhtw.mongodb.net/follower?retryWrites=true&w=majority';
+    constructor ()
+    {
         this.settings();
         this.connect();
     }
 
-    async connect() {
-        try {
-            await Mongoose.connect(this.URI);
-            console.log("Base de datos conectada");
-        } catch (error) {
-            console.log(error);
+    async connect ()
+    {
+        try
+        {
+            await Mongoose.connect( this.URI );
+            console.log( "Base de datos conectada" );
+        } catch ( error )
+        {
+            console.log( error );
         }
     }
 
-    settings() {
-        Mongoose.set("useFindAndModify", false);
-        Mongoose.set("useCreateIndex", true);
-        Mongoose.set("useNewUrlParser", true);
-        Mongoose.set("useUnifiedTopology", true);
+    settings ()
+    {
+        Mongoose.set( "useFindAndModify", false );
+        Mongoose.set( "useCreateIndex", true );
+        Mongoose.set( "useNewUrlParser", true );
+        Mongoose.set( "useUnifiedTopology", true );
     }
 }
