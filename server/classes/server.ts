955a2e7ec9e271b0f00e490e17681c33debca4b1
Ari Vincenti <arivincenti@gmail.com>
2020-02-26 12:26:15 -0300

    modificacion en bbdd

--- server/classes/server.ts
+++ server/classes/server.ts
@@ -8,62 +8,71 @@ import http from "http";
 import { SERVER_PORT } from "../config/environment";
 import * as clientsSocket from "../sockets/clientsSocket";
 
-export default class Server {
+export default class Server
+{
     private app: express.Application;
     private static _instance: Server;
     private port: number | string;
     public io: socketIO.Server;
     private httpServer: http.Server;
 
-    private constructor() {
+    private constructor ()
+    {
         this.app = express();
         this.port = SERVER_PORT;
         this.settings();
         this.middlewares();
         this.routes();
 
-        this.httpServer = new http.Server(this.app);
-        this.io = socketIO(this.httpServer);
+        this.httpServer = new http.Server( this.app );
+        this.io = socketIO( this.httpServer );
 
         this.escucharSocket();
     }
 
-    public static get instance() {
-        return this._instance || (this._instance = new this());
+    public static get instance ()
+    {
+        return this._instance || ( this._instance = new this() );
     }
 
-    settings() {
-        this.app.set("port", this.port);
+    settings ()
+    {
+        this.app.set( "port", this.port );
     }
 
-    middlewares() {
-        this.app.use(cors({ origin: true, credentials: true }));
-        this.app.use(express.json());
-        this.app.use(fileUpload());
+    middlewares ()
+    {
+        this.app.use( cors( { origin: true, credentials: true } ) );
+        this.app.use( express.json() );
+        this.app.use( fileUpload() );
     }
 
-    routes() {
-        this.app.use("/api", routes);
+    routes ()
+    {
+        this.app.use( "/api", routes );
     }
 
-    private escucharSocket() {
-        this.io.on("connection", socket => {
+    private escucharSocket ()
+    {
+        this.io.on( "connection", socket =>
+        {
             //Conect client
-            clientsSocket.connectClient(socket);
+            clientsSocket.connectClient( socket );
             //Confi Client
-            clientsSocket.config_client(socket);
+            clientsSocket.config_client( socket );
             //Join client ticket
-            clientsSocket.joinTicket(socket, this.io);
+            clientsSocket.joinTicket( socket, this.io );
             //Leave client ticket
-            clientsSocket.leaveTicket(socket, this.io);
+            clientsSocket.leaveTicket( socket, this.io );
             //Disconnect client
-            clientsSocket.desconectar(socket);
-        });
+            clientsSocket.desconectar( socket );
+        } );
     }
 
-    async start() {
-        await this.httpServer.listen(this.app.get("port"));
-        console.log(`Server corriendo en el puerto ${this.app.get("port")}`);
+    async start ()
+    {
+        await this.httpServer.listen( this.app.get( "port" ) );
+        console.log( `Server corriendo en el puerto ${ this.app.get( "port" ) }` );
 
         new Database();
     }
