1e5a094ef858389c20fcd5fb1e20472bf5da474c
Ari Vincenti <arivincenti@gmail.com>
2020-03-20 13:50:11 -0300

    cors

--- dist/classes/server.js
+++ dist/classes/server.js
@@ -50,7 +50,7 @@ class Server {
     middlewares() {
         this.app.use(cors_1.default({
             origin: [
-                "https://arivincenti.github.io",
+                "https://arivincenti.github.io/",
                 "http://localhost:4200"
             ],
             credentials: true
--- server/classes/server.ts
+++ server/classes/server.ts
@@ -43,7 +43,7 @@ export default class Server {
         this.app.use(
             cors({
                 origin: [
-                    "https://arivincenti.github.io",
+                    "https://arivincenti.github.io/",
                     "http://localhost:4200"
                 ],
                 credentials: true
