9fbb679ee3fe0fb633f9fbca6f0a7bb6f4287bce
Ari Vincenti <arivincenti@gmail.com>
2020-04-08 16:26:09 -0300

    bbdd

--- dist/database/database.js
+++ dist/database/database.js
@@ -13,10 +13,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 class Database {
-    // private URI: string =
-    //     "mongodb+srv://follower:amFjt2afBZ4wj5ru@cluster0-jfhtw.mongodb.net/follower?retryWrites=true&w=majority";
     constructor() {
-        this.URI = "mongodb://localhost:27017/follower";
+        // private URI: string = "mongodb://localhost:27017/follower";
+        this.URI = "mongodb+srv://follower:amFjt2afBZ4wj5ru@cluster0-jfhtw.mongodb.net/follower?retryWrites=true&w=majority";
         this.settings();
         this.connect();
     }
