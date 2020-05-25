336937301562cba3e94396b27c35094ab33668a2
Ari Vincenti <arivincenti@gmail.com>
2020-05-25 14:20:21 -0300

    se agrego nodemon

--- dist/database/database.js
+++ dist/database/database.js
@@ -13,9 +13,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 class Database {
+    //private URI: string ="mongodb+srv://follower:amFjt2afBZ4wj5ru@cluster0-jfhtw.mongodb.net/follower?retryWrites=true&w=majority";
     constructor() {
-        // private URI: string = "mongodb://localhost:27017/follower";
-        this.URI = "mongodb+srv://follower:amFjt2afBZ4wj5ru@cluster0-jfhtw.mongodb.net/follower?retryWrites=true&w=majority";
+        this.URI = "mongodb://localhost:27017/follower";
         this.settings();
         this.connect();
     }
@@ -23,6 +23,7 @@ class Database {
         return __awaiter(this, void 0, void 0, function* () {
             try {
                 yield mongoose_1.default.connect(this.URI);
+                console.log(this.URI);
                 console.log("Base de datos conectada");
             }
             catch (error) {
